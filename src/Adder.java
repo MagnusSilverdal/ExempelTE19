558dbf9c51f25d8cee4a3a9d75633543df81ee03
MagnusSilverdal <magnus.silverdal@ga.ntig.se>
2020-10-09 10:42:02 +0200

    Initial commit

---
+++ src/Adder.java
@@ -0,0 +1,12 @@
+/**
+ * This is a class
+ * Created 2020-10-09
+ *
+ * @author Magnus Silverdal
+ */
+public class Adder {
+    public static void main(String[] args) {
+        System.out.println("2 + 3 = " + (2+3));
+    }
+
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * This is a class
+ * Created 2020-10-09
+ *
+ * @author Magnus Silverdal
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hej världen");
+    }
+
+}
